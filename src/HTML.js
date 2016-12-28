d64e44fde2b309f6c7ebcc346a62525b5085d3fa
Alejandro <alejandro@estilo.com.ar>
2016-12-28 14:40:34 -0300

    Modificacion de datos usando prompt de javacript

--- src/HTML.js
+++ src/HTML.js
@@ -51,49 +51,24 @@ export function EditContainer( props ) {
 export class EditRow extends Component {
 	constructor( props ) {
 		super( props );
-		this.typingTimer 		= null;
-		this.noChangesTimer		= null;
-		this.doneTypingInterval = 15000,
 		this.root 				= props.root;
 		this.state 				= { model: props.model, edit: false };
-		this.onChange 			= this.onChange.bind( this );
 		this.onClick 			= this.onClick.bind( this );
-		this.doneTyping 		= () => { this.setState({ edit: false }); }
 	}
-	onClick() {
-		this.setState({ edit: true });
-		this.noChangesTimer = setTimeout(()=>{
-			if (this.state.edit) {
-				this.setState({ edit: false });
+	onClick(e) {
+		if (this.root.state.adminMode) {
+			var model = this.props.model;
+			var validator = 'isValid' + this.props.inputKey.toUpperCase();
+			var data = prompt("Modificar", model[this.props.inputKey]);
+			if (!Validator.hasOwnProperty(validator) || Validator[validator]( data )) {
+				model[this.props.inputKey] =  data;
+				this.setState({ model: model });
 			}
-		},15000);
-	}
-	onChange(e) {
-		var model = this.props.model;
-		clearTimeout( this.typingTimer );
-		clearTimeout( this.noChangesTimer );		
-
-		var validator = 'isValid' + this.props.inputKey.toUpperCase();
-		this.props.root.setState({ listenBroadcastUpdate: false, edit: true });
-		if (!Validator.hasOwnProperty(validator) || Validator[validator]( e.target.value )) {
-			model[this.props.inputKey] =  e.target.value;
-			this.typingTimer = setTimeout( this.doneTyping, this.doneTypingInterval );
-			this.setState({ model: model });
-		}		
+		}
 	}
 	render() {
-		let itemEdit; 
-		if ( this.props.root.state.adminMode ) {
-			itemEdit = <input type="text" 
-						  	  onChange={ this.onChange } 
-						  	  value={ this.state.model[this.props.inputKey] } />
-
-		}
-		else {
-			itemEdit = <Link to={this.props.link}>{ this.state.model[this.props.inputKey] }</Link>;
-		}
 		return (
-			<td><div> { itemEdit } </div></td>
+			<td><a href="javascript:void(0)" onClick={this.onClick}>{ this.state.model[this.props.inputKey] }</a></td>
 		);
 	}
 };
